ac15ad487477a599a248bb2b921e59617932d582
Edwin Roosevelt <b.edwinroosevelt@gmail.com>
2021-12-23 13:08:28 +0530

    initial superheroes commit

---
+++ superheroesREST/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for superheroesREST project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superheroesREST.settings')
+
+application = get_asgi_application()
