6b56c64507c5ae2b50f0b05d8b1abc9397632507
Edwin Roosevelt <bedwinroosevelt@gmail.com>
2021-12-24 11:20:44 +0530

    add Superheroes serializer

--- superheroes/views.py
+++ superheroes/views.py
@@ -1,15 +1,28 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
-class superheroes(APIView):
+from serializers import SuperHeroesSerializer
 
-    def get(self, request, format=None):
-        superheroes = [
+superheroes = [
             {
                 "name": "Superman",
                 "universe": "DC"
             }
         ]
-        
+
+
+class superheroes(APIView):
+
+    def get(self, request, format=None):
         return Response({"superheroes": superheroes})
+
+    def post(self, request):
+        serializer = SuperHeroesSerializer(data=request)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'value received'
+            return Response({'message': message})
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
         
